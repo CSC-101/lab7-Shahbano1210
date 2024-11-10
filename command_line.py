47232f9d058bad67bbf5baf7e1dc1375b59bfbb3
Shahbano1210 <saurangz@calpoly.edu>
2024-11-10 15:53:39 -0800

    part 1-3

--- command_line.py
+++ command_line.py
@@ -1,1 +1,19 @@
+import sys
+import convert
 
+def command_line_gather(s:list[str])->list[float]:  # takes in a list of the parameters in the configuration and
+    # returns a new list of the values that were converted to float
+    nums = []
+    # convert sys values to float, sum, print
+    for i in s[1:]:
+        nums.append(i)
+    for i in range(len(nums)):
+        nums[i] = convert.str_to_float(nums[i])
+        if nums[i]==None:
+            nums.remove(nums[i])
+    return nums
+
+
+
+if __name__ == '__main__':
+    print (sum(command_line_gather(sys.argv)))
--- convert.py
+++ convert.py
@@ -1,1 +1,8 @@
+from typing import Optional
 
+# Task 1
+def str_to_float(input:str)-> Optional[float]: #takes in an input str and returns a converted float
+    try:
+        return float(input)
+    except ValueError:
+        return None
